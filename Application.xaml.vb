8c71b0d3ec8d74fe6597e5fd1d8aa90488f42075
jemma <justpromptiguess@gmail.com>
2021-02-17 01:25:29 +0000

    aw ye we getting somewherehre now

--- Application.xaml.vb
+++ Application.xaml.vb
@@ -1,4 +1,7 @@
 ﻿Class Application
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs)
+        Debug.Write("a")
+    End Sub
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,3 +1,10 @@
 ﻿Class MainWindow
+    Private Sub App_Startup()
+        Debug.Write("a")
+    End Sub
 
+    Private Async Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim btc As New coinchecker.btc
+        coinprice.Text = Await btc.price
+    End Sub
 End Class
