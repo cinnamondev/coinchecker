d3da542a03f75cb3bf57ce77b4c18afbeb13f9a1
cinnamon <justpromptiguess@gmail.com>
2021-03-25 13:01:16 +0000

    replace with avalonia

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace coinchecker.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
